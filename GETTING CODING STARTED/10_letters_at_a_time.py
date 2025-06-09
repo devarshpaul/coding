3f883a5624a86263c007f03d1fe64165239d6fc4
devarshpaul <pauldevarsh@gmail.com>
2025-06-09 11:25:59 +0530

    Delete GETTING CODING STARTED directory

--- GETTING CODING STARTED/10_letters_at_a_time.py
+++
@@ -1,9 +0,0 @@
-sentence = 'There once lived a bee in a house by the sea'
-position = 0
-while position <= len(sentence)-10:
-    index = position
-    while index < position+10:
-        print(sentence[index], end='')
-        index += 1
-    print()
-    position += 1
--- GETTING CODING STARTED/divisible_check.py
+++
@@ -1,8 +0,0 @@
-value1=int(input("Enter a starting number: "))
-value2=int(input("Enter a ending number: "))
-divide=int(input("What number to check divisibility for: "))
-num=value1
-while num <= value2:
-    if num % divide == 0:
-        print(num, "is divisible by", divide)
-    num+=1
